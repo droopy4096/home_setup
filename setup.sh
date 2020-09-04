e68402f8245d86d242e345f9b428c2ff648f1439
Dmitry Makovey <dmitry.makovey@rseg.com>
2020-09-04 10:20:48 -0600

    add autocompletions to the mix

--- setup.sh
+++ setup.sh
@@ -10,6 +10,16 @@ link_file(){
   [ -L ${dest_dir}/${linked_file} ] || ln -s $1 ${dest_dir}/${linked_file}
 }
 
+bashrc_install(){
+  include_file=${1}
+  if grep -qF '. ${HOME}/'"${include_file}" ~/.bashrc
+  then
+    echo "${include_file} already installed in ${HOME}/.bashrc"
+  else
+    echo '. ${HOME}/'"${include_file}" >> ~/.bashrc
+  fi
+}
+
 link_file ${BASH_HOME}/.colors
 link_file ${BASH_HOME}/.bash_colors
 link_file ${BASH_HOME}/.bash_prompt
@@ -18,14 +28,11 @@ link_file ${BASH_HOME}/.bash_git
 link_file ${BASH_HOME}/.bash_svn
 link_file ${BASH_HOME}/.xpath.py
 link_file ${BASH_HOME}/.kube_helper
+link_file ${BASH_HOME}/.bash_autocompletions
 
 link_file ${BASH_HOME}/bin/k ${HOME}/bin
 link_file ${BASH_HOME}/bin/k9s ${HOME}/bin
 link_file ${BASH_HOME}/bin/h ${HOME}/bin
 
-if grep -qF '. ${HOME}/.bash_autoprompt' ~/.bashrc
-then
-  echo "autoprompt already installed"
-else
-  echo '. ${HOME}/.bash_autoprompt' >> ~/.bashrc
-fi
+bashrc_install .bash_autoprompt
+bashrc_install .bash_autocompletions
