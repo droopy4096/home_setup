62c8645c5e3c8d1b53e465eaf0232cb28f8ca806
Dmitry Makovey <dmakovey@gitlab.com>
2023-10-04 12:13:10 -0600

    add JSON formatting and folding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,7 @@ require("config.lazy")
 
 -- vim.cmd.colorscheme("catppuccin-mocha")
 vim.cmd.colorscheme("moonfly")
+
+vim.api.nvim_create_user_command("JSONFormat", "<line1>,<line2>!python -m json.tool", {
+  range = true,
+})
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "json",
+  command = [[
+    set foldmethod=expr
+    set foldexpr=nvim_treesitter#foldexpr()
+  ]],
+})
