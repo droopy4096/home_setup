9575a5c9c0822e3bbde0bea257aa11a79f9281bf
Dmitry Makovey <dmakovey@gitlab.com>
2023-10-16 07:59:54 -0600

    get rid of dynamic renumbering in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ require("config.lazy")
 
 -- vim.cmd.colorscheme("catppuccin-mocha")
 vim.cmd.colorscheme("moonfly")
+vim.wo.relativenumber = false
 
 vim.api.nvim_create_user_command("JSONFormat", "<line1>,<line2>!python -m json.tool", {
   range = true,
