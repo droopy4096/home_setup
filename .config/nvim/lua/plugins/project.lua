c7e548b0b6a16f8ed864010d0e039d9afd9ab312
Dmitry Makovey <dmakovey@gitlab.com>
2024-11-20 09:56:37 -0800

    update Vim configs

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -3,6 +3,10 @@ return {
     "ahmedkhalf/project.nvim",
     config = function()
       require("project_nvim").setup()
+      require('telescope').load_extension('projects')
     end,
+    keys = {
+      { "<leader>P", function() require('telescope').extensions.projects.projects() end, desc = "Telescope: Projects" },
+    }
   },
 }
