1e3a33574be81d3f602e0a5041151ad3ed0ffd19
Dmitry Makovey <dmakovey@gitlab.com>
2024-11-21 08:08:48 -0800

    tinkering with outline options

--- .config/nvim/lua/plugins/outline.lua
+++ .config/nvim/lua/plugins/outline.lua
@@ -1,3 +1,79 @@
+--[[ return {
+  'hedyhli/outline.nvim',
+  version = "*", 
+  event = "VeryLazy",
+  config = function()
+    local actions = require('telescope.actions')
+    local action_state = require('telescope.actions.state')
+    local pickers = require('telescope.pickers')
+    local finders = require('telescope.finders')
+    local conf = require('telescope.config').values
+
+    config = require("outline.config")
+    local symbol_types = {}
+    for symbol_type, _ in pairs(config.defaults.symbols.icons) do
+      table.insert(symbol_types, symbol_type)
+    end
+
+    custom_options = {
+      outline_window = {
+        position = 'right',
+        width = 40,
+        relative_width = false,
+      },
+    }
+
+    local outline = require("outline")
+    local function outline_picker()
+      pickers.new({}, {
+        prompt_title = "Filter Outline Type",
+        finder = finders.new_table {
+          results = symbol_types,
+        },
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+
+            if selection and selection.value then
+              local selected_value = selection.value
+              outline.setup(vim.tbl_deep_extend('force', {}, custom_options, {
+                symbols = {
+                  filter = {selected_value}
+                }
+              }))
+              outline.toggle_outline()
+            else
+              print("No selection was made or selection is nil")
+            end
+          end)
+          return true
+        end,
+      }):find()
+    end
+    local function default_outline()
+      outline.setup(vim.tbl_deep_extend('force', {}, custom_options, { symbols = { filter = nil } }))
+      outline.toggle_outline()
+    end
+    vim.api.nvim_create_user_command('OutlineFilter', function()
+      outline_picker()
+    end, {})
+    vim.api.nvim_create_user_command('OutlineDefault', function()
+      default_outline()
+    end, {})
+  end,
+  cmd = { "Outline", "OutlineOpen" },
+  keys={
+      { "<leader>Of", "<cmd>OutlineFilter<CR>", desc = "Outline filter" },
+      { "<leader>Od", "<cmd>OutlineDefaults<CR>", desc = "Outline defaults" },
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  }
+}
+--]]
+
+--[[
+--]]
 return {
   {
     "hedyhli/outline.nvim",
@@ -11,3 +87,5 @@ return {
     },
   },
 }
+--[[
+--]]
