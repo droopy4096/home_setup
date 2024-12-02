8d26434143bcdff158c8e607454641a642068647
Dmitry Makovey <dmakovey@gitlab.com>
2024-12-02 14:24:52 -0800

    Disable non-working git repo identification code

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,19 @@
   -- the opts function can also be used to change the default opts:
+
 return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
+      -- FIXME: Not working code:
+      -- local function git_root()
+      --   return vim.fn.finddir('.git', vim.fn.expand("%:h") .. ";")
+      --   -- return vim.fn.finddir('.git', ".;")
+      -- end
       table.insert(opts.sections.lualine_x, "filetype")
       -- table.insert(opts.sections.lualine_x, "g:syntax")
       -- remove redundant filename info 
-      opts.sections.lualine_c = {}
-      -- opts.inactive_sections.lualine_c = {}
+      -- FIXME: 
+      -- opts.sections.lualine_c = { git_root }
+      opts.inactive_sections.lualine_c = {}
     end,
   }
