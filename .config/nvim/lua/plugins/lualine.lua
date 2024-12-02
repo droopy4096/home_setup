91f311467e9decb1f4677d9fd6c4cd9017ef46a1
Dmitry Makovey <dmakovey@gitlab.com>
2024-12-02 13:33:11 -0800

    Remove UI redundancy

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,5 +5,8 @@ return {
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, "filetype")
       -- table.insert(opts.sections.lualine_x, "g:syntax")
+      -- remove redundant filename info 
+      opts.sections.lualine_c = {}
+      -- opts.inactive_sections.lualine_c = {}
     end,
   }
