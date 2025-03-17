e84b3f83d9576094323268843551b80f97998f1f
Dmitry Makovey <dmakovey@gitlab.com>
2025-03-17 12:08:47 -0700

    Add Karen-yank
    add karen-yank
    latest plugin digest

---
+++ .config/nvim/lua/plugins/karen-yank.lua
@@ -0,0 +1,29 @@
+return {
+  "tenxsoydev/karen-yank.nvim",
+  enabled = true,
+  config = function ()
+      require("karen-yank").setup {
+        mappings = {
+          -- karen controls the use of registers (and probably talks to the manager when things doesn't work as intended)
+          -- map something like `<leader><leader>` if you use the plugin inverted
+          karen = "y",
+          -- false: delete into black hole by default and use registers with karen key
+          -- true: use registers by default and delete into black hole with karen key
+          invert = false,
+          -- disable all if `true` or a table of keymaps [possible values: {"s"|"S"|"d"|"D"|"c"|"C"|"x"|"X"|"p"|"P"|"y"|"Y"}]
+          -- "s"/"S" is not mapped by default, due to it's common utilization for plugins like surround or hop
+          disable = { "s", "S", "d", "D"},
+        },
+        number_regs = {
+          -- use number registers for yanks and cuts
+          enable = true,
+          deduplicate = {
+            -- prevent populating multiple number registers with the same entries
+            enable = true,
+            -- will see `yD` pressed at the beginning of a line as a duplicate of `ydd` pressed in the same line
+            ignore_whitespace = true,
+          },
+        },
+      }
+  end
+}
